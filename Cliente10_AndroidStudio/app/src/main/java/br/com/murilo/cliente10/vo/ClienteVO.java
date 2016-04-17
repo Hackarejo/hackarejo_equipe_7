a62d1ed50148e620cf43a77d084bb3355fa42b1a
andrey9292 <andrey.Guth@Gmail.com>
2016-04-17 01:52:50 -0300

    tree commit

---
+++ Cliente10_AndroidStudio/app/src/main/java/br/com/murilo/cliente10/vo/ClienteVO.java
@@ -0,0 +1,57 @@
+package br.com.murilo.cliente10.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ClienteVO implements Serializable{
+
+    private String nome;
+
+    private String login;
+
+    private String senha;
+
+    private Date data;
+
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
